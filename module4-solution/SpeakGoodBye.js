ae90a771bb95cc6c28461b055fc22073b615e902
TRABU01 <135324104+Hoang-Cam@users.noreply.github.com>
2023-06-20 19:20:01 +0700

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,1 +1,10 @@
-(function (window) { var speakWord = “Good Bye”; // use " or ’ instead of “ and ” function speak(name) { console.log(speakWord + " " + name); } window.speak = speak; })(window);
+// This is an IIFE
+(function () {
+  // Your code goes here
+  var speakWord = "Good Bye"; // This variable is not attached to the byeSpeaker object
+  function speak(name) {
+    console.log(speakWord + " " + name); // This function can access the speakWord variable from the scope
+  }
+  // You can expose some variables or functions to the global scope if needed
+  window.byeSpeaker = byeSspeaker;
+})();
