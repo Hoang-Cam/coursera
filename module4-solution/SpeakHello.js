5b4ae639207fcea969f08708b69f6d6dff0be557
TRABU01 <135324104+Hoang-Cam@users.noreply.github.com>
2023-06-20 19:12:46 +0700

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,1 +1,31 @@
-(function (window) { var speakWord = “Hello”; // use " or ’ instead of “ and ” function speak(name) { console.log(speakWord + " " + name); } window.speak = speak; })(window);
+// This is an IIFE
+(function () {
+  // Your code goes here
+  var speakWord = "Hello";
+  function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+  // You can expose some variables or functions to the global scope if needed
+  window.helloSpeaker = speak;
+})();
+// Create an empty object
+var helloSpeaker = {};
+// Define the speakWord variable
+var speakWord = "Hello";
+// Define the speak method and attach it to the helloSpeaker object
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+};
+// Expose the helloSpeaker object to the global context
+window.helloSpeaker = helloSpeaker;
+// Using dot notation
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+};
+
+// Using bracket notation
+helloSpeaker["speak"] = function (name) {
+  console.log(speakWord + " " + name);
+};
+// Expose the helloSpeaker object to the global scope
+window.helloSpeaker = helloSpeaker;
