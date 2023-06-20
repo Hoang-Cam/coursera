e1cbb4c6d396bec178619a72a0de22e8f74bda07
TRABU01 <135324104+Hoang-Cam@users.noreply.github.com>
2023-06-20 18:47:14 +0700

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,1 +1,1 @@
-(function (window) { var speakWord = “Hello”; function speak(name) { console.log(speakWord + " " + name); } window.speak = speak; })(window);
+(function (window) { var speakWord = “Hello”; // use " or ’ instead of “ and ” function speak(name) { console.log(speakWord + " " + name); } window.speak = speak; })(window);
