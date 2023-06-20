46e0f068c360976d7ac4057616095df6da0caf49
TRABU01 <135324104+Hoang-Cam@users.noreply.github.com>
2023-06-20 19:28:01 +0700

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,31 +1,14 @@
 // This is an IIFE
 (function () {
   // Your code goes here
+  // Create an empty object
+  var byeSpeaker = {};
+  // Define the speakWord variable
   var speakWord = "Goodbye";
-  function speak(name) {
+  // Define the speak method and attach it to the byeSpeaker object
+  byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
-  }
-  // You can expose some variables or functions to the global scope if needed
-  window.byeSpeaker = speak;
+  };
+  // Expose the byeSpeaker object to the global scope
+  window.byeSpeaker = byeSpeaker;
 })();
-// Create an empty object
-var byeSpeaker = {};
-// Define the speakWord variable
-var speakWord = "Goodbye";
-// Define the speak method and attach it to the helloSpeaker object
-byeSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-};
-// Expose the helloSpeaker object to the global context
-window.byeSpeaker = byeSpeaker;
-// Using dot notation
-byeSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-};
-
-// Using bracket notation
-byeSpeaker["speak"] = function (name) {
-  console.log(speakWord + " " + name);
-};
-// Expose the helloSpeaker object to the global scope
-window.byeSpeaker = byeSpeaker;
